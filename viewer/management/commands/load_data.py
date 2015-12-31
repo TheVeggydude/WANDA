9e3026a76ea57e602cacb1de92336be898fc35ce
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2015-12-31 21:02:32 +0100

    Worked on the load_data.py script a bit more, still not functional :(

--- viewer/management/commands/load_data.py
+++ viewer/management/commands/load_data.py
@@ -9,18 +9,62 @@ class Command(BaseCommand):
     # Removes all previous data from the database
     def _remove_data(self):
         Answer.objects.all().delete()
-        Node.objects.all().delete()
+        Question.objects.all().delete()
         Tree.objects.all().delete()
+    
+    
+    # Recursively transforms nodes in the dictionary to a Question object for in the tree
+    def json_to_node(self, dict):
+        
+        #TODO This is sort of a hack in my opinion... Reconsider the whole node/question model thing?
+        # If you are looking at an end node in stead of a normal question only add question text
+        if 'End' in dict:
+            leaf, created = Question.objects.get_or_create(text = dict['End'])
+            if created:
+                # Initial save to create object in database
+                leaf.save()
+            return leaf
+        
+        # Try to find an existing question object with the same question, or create a new one if it doesn't exist yet
+        question, created = Question.objects.get_or_create(text = dict['Question'])
+        
+        # It is was already created we need not do anything but return the object so we can have
+        # Multiple questions referring to the same question
+        if created:
+            # Initial save to create object in database
+            question.save()
+            
+            # Create Answer objects for all the answers and add them to the question object
+            # At the same time recursively get a new question object related to that answer
+            answer_set = []
+            children_set = []
+            for answer_dict in dict['Answers']:
+                answer = Answer(text= answer_dict['Text'])
+                answer.save()
+                answer_set.append(answer)
+                
+                child_question = self.json_to_node(answer_dict['Node'])
+                children_set.append(child_question)
+                
+            question.answers = answer_set
+            question.children = children_set
+            
+            # Save again as there have been (numeral) changes
+            question.save()
+                
+        return question
     
     
     # Creates all the data by reading from a hard coded location
     def _create_data(self):
         
         # Creates a dictionary that needs to be translated to a set of objects
-        tree = utils.read_tree_from_file('../data/data.json')
+        dict = utils.read_tree_from_file('../data/data.json')
         
         # Recursively create Question objects from the dictionary
-        
+        tree = Tree(start = self.json_to_node(dict['Tree']))
+        tree.save()
+    
     
     # Main command handler - denotes the sub-steps taken
     def handle(self, *args, **options):
--- viewer/models.py
+++ viewer/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
 class Answer(models.Model):
-    text = models.CharField(max_length=400, default='answer')
+    text = models.CharField(max_length=400, default='Please update this answer text.')
 
 class Question(models.Model):
     children = models.ForeignKey('self', blank=True, null=True, related_name='parent')
-    answer = models.ForeignKey(Answer)
+    answers = models.ForeignKey(Answer, null=True)
+    text = models.CharField(max_length=400, default='Please update this question text.')
+    
+class Tree(models.Model):
+    start = models.ForeignKey(Question)
+
+'''
+class Answer(models.Model):
+    text = models.CharField(max_length=400, default='answer')
+    next = models.ForeignKey(Question)
+
+class Question(models.Model):
+    answers = models.ForeignKey(Answer)
     text = models.CharField(max_length=200, default='question')
     
 class Tree(models.Model):
     start = models.ForeignKey(Question)
+'''
