b3b4dadfe5234456313ef4b23ba948dcf0f19c7c
jeroen <jeroen@jeroen-desktop>
2016-01-03 15:19:39 +0000

    removed unused url from urls.py

--- wanda/urls.py
+++ wanda/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^questionnaire/', views.question, name='questionnaire'),
     url(r'^$', views.index, name='index'),
-    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
 
 ]
 
