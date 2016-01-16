579e5b11e2b5bb3bd56d7e41d8564b8b72223714
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2016-01-16 17:27:31 +0100

    - removed debug statements - added check to see if leaf node was reached - leaf nodes now render in a different page - result.html - added comments to views.py for documentation

--- viewer/views.py
+++ viewer/views.py
@@ -2,41 +2,49 @@ from django.shortcuts import render
 from viewer.models import *
 
 
-# Create your views here.
+# Simple, just return the standard home page of the site.
 def index(request):
     return render(request, 'viewer/index.html')
 
-
+# Either returns a page with a question and answers or a page showing the final result arrived at.
 def question(request):
     
+    # Create an empty context dictionary used for transferring data to the template engine.
     context = {}
 
-    # If start, then select the first question in the tree to be shown
+    # If start, then select the first question in the tree to be shown.
     if 'start' in request.POST:
         context['question'] = Tree.objects.all().first().start
     
         return render(request, 'viewer/question.html', context)
 
-    # Not a start node, so get the answer text and find the corresponding question
+    # Not a start node, so get the answer text and find the corresponding question.
     question = Question.objects.all().get(id = request.POST['question'])
+
+    # This piece of code finds the child node that corresponds with the chosen answer.
     counter = 0
     for answer in question.answer_set.all():
         counter +=1
         if answer.text == request.POST['choice']:
             break
 
-    print(counter)
-    for child in question.children.all():
+    child = None
+    for current_child in question.children.all():
         counter-=1
         if counter==0:
-            context['question'] = child
+            child = context['question'] = current_child
+            break
 
-
+    # If child has no children, you must be at a leaf node and a result so show the correct page for it.
+    if not child.children.all().exists():
+        return render(request, 'viewer/result.html', context)
+    
+    # Else just render the normal question page.
     return render(request, 'viewer/question.html', context)
 
 
-#Error Responses:
-
+# Error Responses:
+# Special functions Django uses to call error pages automatically when needed.
 
 def handler404(request):
     return render(request,
